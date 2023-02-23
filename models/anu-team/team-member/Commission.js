b6daa7a4a8d7d461ba1eee72e944c9a6faed3c4d
iamshiv007 <iamshiv20032003@gmail.com>
2023-02-23 11:24:41 +0530

    anu-team modified

--- models/anu-team/team-member/Commission.js
+++ models/anu-team/team-member/Commission.js
@@ -12,13 +12,25 @@ const commissionSchema = new Schema(
     service_commission:{
         type:String
     },
+    service_commission_type:{
+        type:String
+    },
     product_commission:{
+        type:String
+    },
+    product_commission_type:{
         type:String
     },
     voucher_commission:{
         type:String
     },
+    voucher_commission_type:{
+        type:String
+    },
     membership_commission:{
+        type:String
+    },
+    membership_commission_type:{
         type:String
     }
   }
